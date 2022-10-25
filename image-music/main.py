22f25fe17e98a3f77121f5da4f28f7cc1db8ee0e
ethanbg2 <ethanbg2@illinois.edu>
2022-10-25 18:06:01 -0500

    added tonejs test to vue app

--- image-music/main.py
+++ image-music/main.py
@@ -1,10 +1,25 @@
 from process_image import ProcessImage
 from color_average import ColorAverage
+import json
+import os
 
 def main():
+
     process = ProcessImage("data/test-art.jpg")
     coords, images = process.divide_image(8)
-    images[coords[100][100]].show()
+
+
+    j = json.dumps(coords.tolist())
+    f_name = 'sample.json'
+    out_dir = "../ui/interactive-art-VUE/src/assets"
+    with open(os.path.join(out_dir, f_name), "w") as outfile:
+        outfile.write(j)
+    
+    # i = 0
+    # for image in images:
+    #     print(i)
+    #     image.show()
+    #     i+=1
 
 if __name__ == "__main__":
     main()
