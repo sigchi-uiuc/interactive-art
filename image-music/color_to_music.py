e41c85adc3ad231025c8e62e0007fc8534d7a516
ethanbg2 <ethanbg2@illinois.edu>
2022-11-30 14:18:04 -0600

    made music flexible to different octaves

--- image-music/color_to_music.py
+++ image-music/color_to_music.py
@@ -2,21 +2,20 @@ import math
 import numpy as np
 
 class Color2Music:
-    COLOR_MAP = {(82, 0, 0): "F4", 
-                (116, 0, 0): "F#4", 
-                (179, 0, 0): "G4", 
-                (238, 0, 0): "G#4",
-                (255,99,0): "A4",
-                (255, 236, 0): "A#4",
-                (153, 255, 0): "B4",
-                (40, 255, 0): "C5",
-                (0, 255, 232): "C#5",
-                (0, 124, 255): "D5",
-                (5, 0, 255): "D#5",
-                (69, 0, 234): "E5",
-                (87, 0, 158): "F5"}
-
-    def __init__(self, rgb):
+    def __init__(self, rgb, octave=4):
+        self.COLOR_MAP = {(82, 0, 0): f"F{octave}", 
+                (116, 0, 0): f"F#{octave}", 
+                (179, 0, 0): f"G{octave}", 
+                (238, 0, 0): f"G#{octave}",
+                (255,99,0): f"A{octave}",
+                (255, 236, 0): f"A#{octave}",
+                (153, 255, 0): f"B{octave}",
+                (40, 255, 0): f"C{octave+1}",
+                (0, 255, 232): f"C#{octave+1}",
+                (0, 124, 255): f"D{octave+1}",
+                (5, 0, 255): f"D#{octave+1}",
+                (69, 0, 234): f"E{octave+1}",
+                (87, 0, 158): f"F{octave+1}"}
         self.rgb = rgb
     
     def get_note(self):
