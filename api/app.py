1cc22a08408bb6aad27db06576f994c67538310e
ethanbg2 <ethanbg2@illinois.edu>
2023-01-05 17:45:06 -0600

    implemented server side cache

--- api/app.py
+++ api/app.py
@@ -1,23 +1,36 @@
-from flask import Flask, jsonify, send_from_directory
+from flask import Flask, jsonify
 from flask_cors import CORS
 from process_image import ProcessImage
 import time
+from flask_caching import Cache
 
-data_dir = "../src/assets"
+# parameters
+DATA_DIR = "../src/assets"
+CACHE_DIR = "cache"
+cache_config = {
+    "DEBUG": True,    
+    "CACHE_TYPE": "FileSystemCache",
+    "CACHE_DEFAULT_TIMEOUT": 0,
+    "CACHE_DIR": CACHE_DIR
+}
 
+# set up flask app
 app = Flask(__name__)
+app.config.from_mapping(cache_config)
 CORS(app)
+cache = Cache(app)
 
 @app.route("/")
 def api():
     return "interactive-art api", 200
 
 @app.route('/coords/<image>/<int:width>/<int:height>/')
+@cache.memoize()
 def get_notes(image, width, height):
     start = time.time()
 
     process = ProcessImage(image, 
-                          data_dir, 
+                          DATA_DIR, 
                           target_width=width, 
                           target_height=height)
     coords, images = process.divide_image()
