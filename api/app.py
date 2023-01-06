6d2ea955e283bd8a225d7196980eabeadcacfbb8
ethanbg2 <ethanbg2@illinois.edu>
2023-01-06 00:25:46 -0600

    created more efficient cache

--- api/app.py
+++ api/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from process_image import ProcessImage
+from extract_color import ExtractColor
+from process_image import SplitImage
+from color_to_music import Color2Music
 import time
+import os
+import numpy as np
 from flask_caching import Cache
 
 # parameters
@@ -13,6 +17,7 @@ cache_config = {
     "CACHE_DEFAULT_TIMEOUT": 0,
     "CACHE_DIR": CACHE_DIR
 }
+NUM_SECTIONS = 8
 
 # set up flask app
 app = Flask(__name__)
@@ -25,21 +30,43 @@ def api():
     return "interactive-art api", 200
 
 @app.route('/coords/<image>/<int:width>/<int:height>/')
-@cache.memoize()
 def get_notes(image, width, height):
     start = time.time()
 
-    process = ProcessImage(image, 
-                          DATA_DIR, 
-                          target_width=width, 
-                          target_height=height)
-    coords, images = process.divide_image()
+    # get primary color from each section of the image
+    # it will be cached if a given image has been seen before with the same number of sections
+    colors = extract_colors(image, NUM_SECTIONS)
+
+    # convert color to music note for each chunk and create a coordinate map with all notes
+    i = 0
+    note_coord = np.ones((width, height), dtype=object) * -1
+    split = SplitImage((width, height), NUM_SECTIONS)
+    for chunk in split.get_chunks():
+        color = colors[i]
+        color2mus = Color2Music(color)
+        note = color2mus.get_note()
+
+        note_coord[chunk[0]:chunk[2], chunk[1]:chunk[3]] = note
+        i += 1
+    
+    if -1 in np.unique(note_coord):
+            print(note_coord)
+            print("image size", width, height)
+            raise Exception("Not all image area was initialized")
 
     end = int(time.time() - start)
     print(f"processed image in: {end} seconds")
 
-    response = {"notes": coords.tolist()}
+    response = {"notes": note_coord.tolist()}
     return jsonify(response), 200
+
+@cache.memoize()
+def extract_colors(f_name, num_split):
+    print("primary image colors not cached")
+    
+    img_path = os.path.join(DATA_DIR, f_name)
+    ext_color = ExtractColor(img_path, num_split)
+    return ext_color.get_color_sections()
 
 if __name__ == '__main__':
     port_num = 5000
--- api/extract_color.py
+++ api/extract_color.py
@@ -1,12 +1,19 @@
 from PIL import Image
+from process_image import SplitImage
 import colorgram
 
 class ExtractColor:
-  def __init__(self, img, num_colors=1):
-    self.colors = colorgram.extract(img, num_colors)
+  def __init__(self, img_file, num_sections):
+    self.img = Image.open(img_file)
+    self.split = SplitImage(self.img.size, num_sections)
+  
+  def get_color_sections(self, num_colors=1):
+    colors = []
+    for chunk in self.split.get_chunks():
+       image_section = self.img.crop(chunk)
 
-  def get_dominant(self):
-    return self.colors[0].rgb
+       color_section = colorgram.extract(image_section, num_colors)
+       dominant_color = color_section[0].rgb
+       colors.append(dominant_color) 
 
-  def get_palette(self):
-    return [color.rgb for color in self.colors]
+    return colors
