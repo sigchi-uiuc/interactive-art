68a55c7d32105c7b4d47104aa9f31e6a066a64e2
Rohith Sanjay <92739890+rohiths2@users.noreply.github.com>
2023-02-07 19:17:16 -0600

    Added info to About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
+<div class="about">
+    <h1>About SIGCHI</h1>
+    <p> SIGCHI is a Special Interest Group within ACM (Association of Computing Machinery), which focuses on Computer-Human Interaction. In weekly meetings (Tuesdays at 6pm), we learn about real-world examples of computer-human interaction in current technology, and then engage in discussions and work on our main project for this year: a tool that produces sound/music from an art/picture, depending on where a person is looking at the artwork. <p>
+    <br>
+
+    <h1>Details About This Project </h1>
+    <p> This project takes in human-interactive input via an eye tracker, uses the input to select an area of an image, and matches the average color of that area to a specific sound. To accomplish this, our SIG was divided into 4 sub-teams: one dedicated for the eye tracker functionality, one for the user interface, one for infra, and one for image-music conversion.</p>
+    <br>
+
+    <h1>Links</h1>
+    <p> ACM Website: https://acm.illinois.edu/ </p>
+    <p> SIGCHI Website: https://sigchi.acm.illinois.edu/ </p>
+    <br>
+    
+    <h1> Info About Members</h1>
+    <p>Lead: Abhimanyu Thosar, Mitchell Bifeld, Swathi Ram, Jeffrey Tsai</p>
+    <p>UI Team: Aditi, Will, Rohith, Karthik, Mari, Ethan, Mitchell</p>
   </div>
-</template>
