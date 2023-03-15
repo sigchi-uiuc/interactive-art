58c43a7aeb96aa3d6bded35ddcfdf5524e2a584d
ethanbg2 <ethanbg2@illinois.edu>
2023-03-14 21:59:34 -0500

    added navbar

--- src/views/CarouselView.vue
+++ src/views/CarouselView.vue
@@ -1,17 +1,32 @@
 <template>
+        <div class="nav-bar">
+            <div class="logo-container">
+                <img :src="require('@/assets/icons/eyes.png')" class="logo">
+                <h1>
+                    Melodic Eyes
+                </h1>
+            </div>
+            <button class="learn-button">
+                Learn More
+            </button>
+        </div>
+        <hr class="divider">
+
         <carousel :itemsToShow="3">
             <slide v-for="(item, index) in art_data" :key="item.file">
-              <div>
-                  <h2>{{item.title}}</h2>
-                  <img class="car_img" :src="require(`@/assets/${item.file}`)">
-                  <p>{{item.artist}}</p>
-                  <p>{{item.date}}</p>
-                      <button class="view-button" @:mouseover="viewArtButtonHover = true; artIndex = index; button_hover=true" 
-                                                  @:mouseleave="viewArtButtonHover = false; button_hover=false"
-                                                  @:click="this.$router.push('/art-view/' + this.artIndex)">
-                          <span :id="'art/' + index"></span>
-                          <div class="view-art-text"> View Art </div>
-                      </button>
+              <div class="card-container">
+                    <h3>{{item.title}}</h3>
+
+                    <img class="car_img" :src="require(`@/assets/${item.file}`)">
+                    <div class="card-content">
+                        <p>{{item.artist + " - " + item.date}}</p>
+                        <button class="view-button" @:mouseover="viewArtButtonHover = true; artIndex = index; button_hover=true" 
+                                                    @:mouseleave="viewArtButtonHover = false; button_hover=false"
+                                                    @:click="this.$router.push('/art-view/' + this.artIndex)">
+                            <span :id="'art/' + index"></span>
+                            <div class="view-art-text"> View Art </div>
+                        </button>
+                    </div>
               </div>
             </slide>
         
@@ -89,10 +104,72 @@ const ART_DATA = require("@/assets/art_data.json")
 </script> 
 
 <style scoped>
-    .car_img {
-        height: 300px;
-        object-fit: contain;
+    .nav-bar {
+        display: flex;  
+        align-items: center;
+        justify-content: space-between;
     }
+
+    .logo-container {
+        display: flex;
+        align-items: center;
+        margin-left: var(--dl-space-space-unit);
+        order: 1;
+    }
+
+    .logo {
+        width: 100px;
+        height: 100px;
+    }
+
+    .learn-button {
+        order: 2;
+        margin-right: var(--dl-space-space-unit);
+
+        background-color: rgba(50, 168, 80, 0.6);
+        font-size: 16px;
+        width: 100px;
+        height: var(--dl-size-size-small);
+        border-width: 3px;
+        border-color: var(--dl-color-gray-500);
+        border-radius: var(--dl-radius-radius-radius8);
+        box-shadow: 0px 4px 2px 0px #595959;
+        position: relative;
+    }
+
+    .divider {
+        margin-left: var(--dl-space-space-unit);
+        margin-right: var(--dl-space-space-unit);
+    }
+
+    .card-container {
+        border-radius: 4px;
+        background-color: #fff;
+        box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+        color: rgba(0,0,0,.87);
+
+        overflow: hidden;
+        position: relative;
+        width: 410px;
+        height: 500px;
+        margin-bottom: var(--dl-space-space-unit);
+        margin-top: var(--dl-space-space-unit);
+    }
+    .car_img {
+        object-fit: cover;
+        height: 70%;
+        width: 100%;
+    }
+
+    .card-content {
+        overflow: hidden;
+        position: absolute; /* Position the text relative to the card container */
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 30%; /* Set the height of the text container */
+        background-color: #fff; /* Set the background color of the text container */
+      }
     
     .view-button {
         margin: var(--dl-space-space-halfunit);
