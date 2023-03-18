d91da1bfa5cd8b55f6f9ce3df347ab1045c874e9
ethanbg2 <ethanbg2@illinois.edu>
2023-03-17 21:01:29 -0500

    added alert for browser autoplay

--- src/components/alert.vue
+++ src/components/alert.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="alert-container">
-      <div class="alert-container1">
+      <div class="alert-container1" :style="{height: alert_height + 'px'}">
         <svg viewBox="0 0 1024 1024" class="alert-icon">
           <path
             d="M470 384v-86h84v86h-84zM512 854q140 0 241-101t101-241-101-241-241-101-241 101-101 241 101 241 241 101zM512 86q176 0 301 125t125 301-125 301-301 125-301-125-125-301 125-301 301-125zM470 726v-256h84v256h-84z"
@@ -17,8 +17,12 @@
     props: {
       text: {
         type: String,
-        default: 'This is an alert',
-      },
+        default: 'Make sure your browser autoplay is on!',
+      }, 
+      alert_height: {
+        type: Number,
+        default: 50
+      }
     },
   }
   </script>
@@ -36,7 +40,6 @@
   .alert-container1 {
     flex: 0 0 auto;
     width: 100%;
-    height: auto;
     display: flex;
     align-items: flex-start;
     border-radius: var(--dl-radius-radius-radius8);
@@ -44,9 +47,9 @@
   }
   .alert-icon {
     fill: #25A6E0;
-    width: 30px;
-    height: 30px;
-    margin: var(--dl-space-space-unit);
+    width: 20px;
+    height: 20px;
+    margin: var(--dl-space-space-halfunit);
     align-self: center;
   }
   .alert-text {
